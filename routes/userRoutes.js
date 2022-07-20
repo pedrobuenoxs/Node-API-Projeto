18cc971e79ff94750aa9025c4efa7888b1b96fb4
Calenzo <calenzodevs@gmail.com>
2022-07-20 15:36:22 -0300

    start project

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router()
+const { createValidator } = require('express-joi-validation')
+const validator = createValidator({});
+const { userQuerySchema, userDefaultBodySchema } = require('../middleware/validator');
+const controller = require('../controllers/userController');
+
+
+
+router.get('/users', validator.query(userQuerySchema), controller.getUser)
+router.post('/users', validator.body(userDefaultBodySchema), controller.createUser)
+router.put('/users', validator.body(userDefaultBodySchema), controller.updateUser)
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const app = express();
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productsRoutes');
+const swaggerFile = require('./swagger/swagger_output.json');
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(express.json());
+app.listen(8080, () => {
+    console.log('Server up...')
+});
+
+
+app.use('/api', userRouter);
+app.use('/api/products', productRouter);
