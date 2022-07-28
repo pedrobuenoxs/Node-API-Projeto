8129d3b17990b95ba35e8f2711ea9094ada54948
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-28 20:33:44 -0300

    add mongoose

--- api/app.js
+++ api/app.js
@@ -7,6 +7,19 @@ const swaggerFile = require("./src/swagger/swagger_output.json");
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    `mongodb+srv://pedrobuenoxs:12345@dev-challenge.wwuxf6z.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Db connected");
+  })
+  .catch((err) => {
+    console.log("Error while connecting database::", err);
+  });
+
 app.use("/api", userRouter);
 
 module.exports = app;
