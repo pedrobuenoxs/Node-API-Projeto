03d095d60d157b5452ca9ee7924c8eb8bc688b71
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-28 22:07:04 -0300

    rewrite ecrypt method

--- api/src/user/entities/Password.js
+++ api/src/user/entities/Password.js
@@ -3,7 +3,7 @@ export class Password {
   password;
   constructor(password) {
     this.id = id;
-    this.password = this.encryptPassword(password);
+    this.password = password;
   }
   encryptPassword() {
     return bcrypt.hashSync(this.password, 10);
